d688611fea836cc3e49e4c00c6fdc51e41c8398f
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-03-15 17:45:06 +0600

    project  initialize

---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+</div>
+
+  <footer class="footeroption">
+	  <section class="footerone">
+          <p>Md Nazmul Hasan</p>
+          <p>Web Application Developer</p>
+	  </section>
+  </footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include "inc/header.php"; ?>
+
+<?php
+
+include 'system/libs/Main.php';
+new Main();
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "inc/footer.php"; ?>
